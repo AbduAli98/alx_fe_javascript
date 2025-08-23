8139c28a93460c0bbbb3a8bba5d0a3d506d68a58
AbduAli98 <abdulrahmanali9814@gmail.com>
2025-08-24 01:35:01 +0300

    updated

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,35 +1,18 @@
 let quotes = [
   { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
-  { text: "Success is not the key to happiness. Happiness is the key to success.", category: "Inspiration" }
+  { text: "Success is not the key to happiness. Happiness is the key to success.", category: "Inspiration" },
+  { text: "Don’t watch the clock; do what it does. Keep going.", category: "Time" }
 ];
 
 const quoteDisplay = document.getElementById("quoteDisplay");
 const newQuoteBtn = document.getElementById("newQuote");
-const addBtn = document.getElementById("addBtn");
-const newQuoteText = document.getElementById("newQuoteText");
-const newQuoteCategory = document.getElementById("newQuoteCategory");
 
-let currentIndex = 0;
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
 
-function showNextQuote() {
-  const q = quotes[currentIndex];
-  quoteDisplay.textContent = `"${q.text}" - ${q.category}`;
-  currentIndex = (currentIndex + 1) % quotes.length; 
-}
-function addQuote() {
-  const text = newQuoteText.value.trim();
-  const category = newQuoteCategory.value.trim();
+  const randomQuote = quotes[randomIndex];
 
-  if (text && category) {
-    quotes.push({ text: text, category: category });
-    newQuoteText.value = "";
-    newQuoteCategory.value = "";
-    alert("Quote added successfully!");
-  } else {
-    alert("Please fill both fields!");
-  }
+  quoteDisplay.innerHTML = `"${randomQuote.text}" <br> <em>- ${randomQuote.category}</em>`;
 }
 
-newQuoteBtn.onclick = showNextQuote;
-
-
+newQuoteBtn.addEventListener("click", showRandomQuote);
